13be008c3c8c9d308e77002f9d0f9a43c46e74c8
sooboi <dlgbrkd123@gmail.com>
2023-08-17 00:08:42 +0900

    Study : React Router useParams, useSearchParams, useNavigate 연습

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,35 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  console.log("id : ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode : ", mode);
+
   return (
     <div>
       <h1>Edit</h1>
-      <p>일기</p>
+      <p>수정</p>
+      <button onClick={() => setSearchParams({ who: "Sueboi" })}>
+        QS 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        돌아가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
