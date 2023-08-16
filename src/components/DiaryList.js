edebebaa71fc1246bbed791e761e310257a02a39
sooboi <dlgbrkd123@gmail.com>
2023-08-17 03:21:43 +0900

    Add : Diary List 정렬 기능 추가 (날짜순,감정순)

---
+++ src/components/DiaryList.js
@@ -0,0 +1,87 @@
+import { useState } from "react";
+
+// 날짜 순 정렬 List
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+// 감정 순 정렬 List
+const filterOptionList = [
+  { value: "all", name: "전부" },
+  {
+    value: "good",
+    name: "좋은 감정만",
+  },
+  { value: "bad", name: "나쁜 감정만" },
+];
+
+// 정렬 기능바 컴포넌트
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType] = useState("latest");
+  const [filter, setFilter] = useState("all");
+
+  // 정렬 기능
+  const getProcessedDiaryList = () => {
+    const filterCallBack = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
+
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it));
+
+    const sortedList = filteredList.sort(compare);
+    return sortedList;
+  };
+
+  return (
+    <div>
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      />
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
+      />
+      {getProcessedDiaryList().map((it) => (
+        <div key={it.id}>
+          {it.content} {it.emotion}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+DiaryList.defaultProps = {
+  diaryList: [],
+};
+export default DiaryList;
